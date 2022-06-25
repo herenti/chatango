d2e0fa1b332677a3eadbb42b8ca1df97b628f7d6
herenti <106109941+herenti@users.noreply.github.com>
2022-06-25 11:25:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import json
 import glob
 import threading
+import text
 from text import room_list, lockrooms
 import imp
 
@@ -14,17 +15,20 @@ if debug == True: room_list = ['the-shire']
 
 class Lemonator(cakelib2.Main):
 
-    def e(self, string, user, uid, chat, cake):
+    def _e(self, string, user, uid, chat, cake):
         if "password" in string.lower(): return 'fail'
         else:
             try:
                 ret = eval(string.decode() if type(string) == bytes else string)
                 return str(repr(ret))
-            except Exception: return str('%s' % get_error())        
+            except Exception:
+                return str('%s' % get_error())        
 
-    def reindex(self, string, user, uid, chat, othervars):
-        try: list(map(lambda x: exec('imp.reload({a})'.format(a=x.replace('.py','') if x.replace('.py','') != 'main' else 'core')), glob.glob('*.py')))
-        except Exception: return '%s' % get_error()
+    def _reindex(self, string, user, uid, chat, othervars):
+        try:
+            list(map(lambda x: exec('imp.reload({a})'.format(a=x.replace('.py','') if x.replace('.py','') != 'main' else 'core')), glob.glob('*.py')))
+        except Exception:
+            return '%s' % get_error()
         return 'Reloaded Modules.'
 
     def onPost(self, user, chat, message):
@@ -37,6 +41,7 @@ class Lemonator(cakelib2.Main):
         user.rank = 2 if user.name.lower() in mods else 1
         othervars = [message, self.pm, self, user]
         if othervars[3].rank < 2 and chat.chatname in lockrooms: return
+        if othervars[3].name in cakelib2._user_: return
         uid = message.uid
         user = user.name.lower()
         prefix = chat.prefix
@@ -61,33 +66,38 @@ class Lemonator(cakelib2.Main):
                 func = func[1:] if _prefix == True else func
             except: _prefix = False
             if _prefix:
+                func = '_'+func
                 if hasattr(self, func):
                     try:
                         if othervars[3].rank < 2:
                             chat.post('You do not have moderator privleges.')
                             return
                         chat.post(getattr(self, func)(string, user, uid, chat, othervars))
-                    except Exception: chat.post(get_error())
+                    except Exception: chat.post('invalid command use')
                 elif hasattr(core, func):
                     try:
                         resp = getattr(core, func)(string, user, uid, chat, othervars)
                         if resp == None: resp = 'incorrect command usage'
                         chat.post(resp)
-                    except Exception: chat.post(get_error())
+                    except Exception: chat.post('invalid command use')
 
     def onPm(self, user, msg, net):
         self.pm.say(user, 'i am a bot')            
         
 def get_error():
     try: et, ev, tb = sys.exc_info()
-    except Exception as e: print(e)
-    if not tb: return None
+    except Exception as e:
+        print(e)
+    if not tb:
+        return None
     while tb:
             line = tb.tb_lineno
             file = tb.tb_frame.f_code.co_filename
             tb = tb.tb_next
-    try: return "%s: %i: %s[%s]" % (file, line, et.__name__, str(ev))
-    except Exception as e: print(e)
+    try:
+        return "%s: %i: %s[%s]" % (file, line, et.__name__, str(ev))
+    except Exception as e:
+        print(e)
 
 def load_utill():
     f = open('utill.txt', 'r')
@@ -104,5 +114,5 @@ if __name__ == '__main__':
         core._timer(180, core.dumpwhois, None)
         Lemonator.start(u, core.decrypt(k, p), room_list, pm=True)
     except:
-        'caek' == 'WATER MALONE'
+        'cake' == 'gacen'
         print(get_error())
